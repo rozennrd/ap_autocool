113bd06f6cbe268da5122a4984b67298cecf2b36
Rozenn RENAUD <rrenaud@geiffel.fr>
2023-03-17 11:59:08 +0100

    ajout démarrage android + fin api

---
+++ Autocool/app/src/main/java/com/example/autocool/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.autocool;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    String responseStr ;
+    OkHttpClient client = new OkHttpClient();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button buttonValiderAuthentification = (Button)findViewById(R.id.btnValider);
+        buttonValiderAuthentification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Appel de la fonction authentification
+                try {
+                    authentification();
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void authentification() throws IOException {
+
+        final EditText textLogin = findViewById(R.id.editTextLogin);
+        final EditText textMdp = findViewById(R.id.editTextPassword);
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("login", textLogin.getText().toString())
+                .add("mdp", textMdp.getText().toString())
+                .build();
+
+        Request request = new Request.Builder()
+                .url("http://192.168.56.1/servWebEtudiant/auhentification.php")
+                .post(formBody)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+
+            public void onResponse(Call call, Response response) throws IOException {
+
+                responseStr = response.body().string();
+
+                if (responseStr.compareTo("false") != 0) {
+                    try {
+                        JSONObject etudiant = new JSONObject(responseStr);
+                        if (etudiant.getString("statut").compareTo("prof") != 0) {
+                            Intent intent = new Intent(MainActivity.this, choixPartie.class);
+                            intent.putExtra("etudiant", etudiant.toString());
+                            startActivity(intent);
+                        }
+                    } catch (JSONException e) {
+                        Log.d("Test", e.getMessage());
+                        // Toast.makeText(MainActivity.this, "Erreur de connexion !!!! !", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Log.d("Test", "Login ou mot de  passe non valide !");
+                }
+            }
+
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.d("Test", "erreur!!! connexion impossible");
+                Log.d("Test", e.getMessage());
+                e.printStackTrace();
+            }
+
+        });
+    }
+}
---
+++ Autocool/app/src/main/java/com/example/autocool/choixPartie.java
@@ -0,0 +1,14 @@
+package com.example.autocool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class choixPartie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choix_partie);
+    }
+}
